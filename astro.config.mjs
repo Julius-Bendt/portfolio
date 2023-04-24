eed27512f0251b5e9cdc544eb8168ef006f03d63
bukz <julius@juto.dk>
2023-04-24 08:56:37 +0200

    Cleaned structure, Implemented portfolio page, Added navbar again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
@@ -10,5 +9,11 @@ import frontendistahtmlMinify from "@frontendista/astro-html-minify";
 // https://astro.build/config
 export default defineConfig({
   site: "https://juto.dk",
-  integrations: [mdx(), sitemap(), vue(), tailwind(), compress(), frontendistahtmlMinify()]
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    compress(),
+    frontendistahtmlMinify(),
+  ],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -3,44 +3,32 @@
 
 export const SITE_TITLE = 'Julius Bendt | Website';
 export const SITE_DESCRIPTION = 'Julius Bendt - Fullstack developer, DevOps enginner and dark mode entusiast';
-export const technologies = [
 
+
+export const techstacks = {
+    "Backend technologies": [
         {
             name: "Laravel",
             icon: "laravel",
-            fillColor:"#FF2D20"
+            fillColor: "#FF2D20"
         },
         {
             name: "Php",
             icon: "php",
-            fillColor:"#777BB4"
+            fillColor: "#777BB4"
         },
         {
             name: ".NET",
             icon: "dotnet",
-            fillColor:"#512BD4"
+            fillColor: "#512BD4"
         },
         {
             name: "C#",
             icon: "csharp",
-            fillColor:"#239120"
+            fillColor: "#239120"
         },
-
-        {
-            name: "Redis",
-            icon: "redis",
-            fillColor:"#DC382D"
-        },
-        {
-            name: "MySQL",
-            icon: "mysql",
-            fillColor:"#4479A1"
-        },
-        {
-            name: "ElasticSearch",
-            icon: "elasticsearch",
-            fillColor:"#005571"
-        },
+    ],
+    "Frontend technologies": [
         {
             name: "Vue",
             icon: "vuedotjs",
@@ -61,7 +49,6 @@ export const technologies = [
             icon: "astro",
             fillColor:"#FF5D01"
         },
-
         {
             name: "Flutter",
             icon: "flutter",
@@ -72,11 +59,62 @@ export const technologies = [
             icon: "dart",
             fillColor:"#0175C2"
         },
-
+    ],
+    "Persistent technologies": [
+         {
+            name: "Redis",
+            icon: "redis",
+            fillColor:"#DC382D"
+        },
+        {
+            name: "MySQL",
+            icon: "mysql",
+            fillColor:"#4479A1"
+        },
+        {
+            name: "ElasticSearch",
+            icon: "elasticsearch",
+            fillColor:"#005571"
+        },
+    ],
+    "Game development": [
         {
             name: "Unity",
             icon: "unity",
             fillColor:"#FFFFFF"
         }
-
     ]
+}
+
+export const portfolio = [
+    {
+        "name": "Stackflock",
+        "description": "Stackflock's objective is to establish connections between developers and companies. It achieves this by providing a map-based interface that allows developers to locate companies within their geographical vicinity and filter them based on their technological stack.<br><br>This feature helps both developers and companies to gain insight into the growing technology trends within their locality.<br><br>The project is still work-in-progress",
+        "href": "https://stackflock.com",
+        "image":"stackflock.png"
+    },
+    {
+        "name": "Graitor",
+        "description": "Graitor is a startup with a mission to foster environmental sustainability globally. The core of Graitor's business model involves incentivizing conscientious user behavior, specifically through the act of \"panting\", or the collection and delivery of empty cans and bottles.<br><br>Users receive points for engaging in this activity, which can be subsequently redeemed for an array of rewards including discounts, unique experiences, and complimentary merchandise",
+        "href": "https://graitor.dk",
+        "image":"graitor.png"
+    },
+    {
+        "name": "Freelance developer",
+        "description": "As a seasoned freelancer, I have acquired ample experience in diverse areas of software development such as game, app, and website development.<br><br>My client base primarily comprised of small and mid-sized enterprises for whom I provided my services. My experience as a freelancer has endowed me with the necessary skills to tackle complex projects and deliver results within the designated timelines.<br><br><b>please notice: I don't do freelance work anymore</b>",
+        "href": null,
+        "image":"freelance.jpg"
+    },
+    {
+        "name": "Game development teacher",
+        "description": "Gamelab is a activity for young people in the municipality of Aalborg who are interested in game development. I have been a member of Gamelab since 2015.<br><br>In 2018, I was hired to teach high school students about game development, specifically in areas such as programming, design, graphics, sound, and time management.",
+        "href": "https://aalborgungdomsskole.dk/gamelab-tirsdag/",
+        "image":"gamelab.png"
+    },
+    {
+        "name": "Power the evolution",
+        "description": "Power The Evolution originated as a high school project, which subsequently progressed beyond its initial delivery stage with the continued involvement of myself and a friend.<br><br>The game received acclaim in the form of inclusion in Denmark's championship for game development and earned the \"Youth Against Global Warming\" award in 2018.",
+        "href": null,
+        "image":"power.png"
+    },
+];
