f73feed754992436d1808f172ab2935efaa1cba7
bukz <julius@juto.dk>
2023-04-24 09:41:09 +0200

    Added itch on homepage, Changed pictures from png/jpg to webp

--- src/consts.ts
+++ src/consts.ts
@@ -91,30 +91,30 @@ export const portfolio = [
         "name": "Stackflock",
         "description": "Stackflock's objective is to establish connections between developers and companies. It achieves this by providing a map-based interface that allows developers to locate companies within their geographical vicinity and filter them based on their technological stack.<br><br>This feature helps both developers and companies to gain insight into the growing technology trends within their locality.<br><br>The project is still work-in-progress",
         "href": "https://stackflock.com",
-        "image":"stackflock.png"
+        "image":"stackflock.webp"
     },
     {
         "name": "Graitor",
         "description": "Graitor is a startup with a mission to foster environmental sustainability globally. The core of Graitor's business model involves incentivizing conscientious user behavior, specifically through the act of \"panting\", or the collection and delivery of empty cans and bottles.<br><br>Users receive points for engaging in this activity, which can be subsequently redeemed for an array of rewards including discounts, unique experiences, and complimentary merchandise",
         "href": "https://graitor.dk",
-        "image":"graitor.png"
+        "image":"graitor.webp"
     },
     {
         "name": "Freelance developer",
         "description": "As a seasoned freelancer, I have acquired ample experience in diverse areas of software development such as game, app, and website development.<br><br>My client base primarily comprised of small and mid-sized enterprises for whom I provided my services. My experience as a freelancer has endowed me with the necessary skills to tackle complex projects and deliver results within the designated timelines.<br><br><b>please notice: I don't do freelance work anymore</b>",
         "href": null,
-        "image":"freelance.jpg"
+        "image":"freelance.webp"
     },
     {
         "name": "Game development teacher",
         "description": "Gamelab is a activity for young people in the municipality of Aalborg who are interested in game development. I have been a member of Gamelab since 2015.<br><br>In 2018, I was hired to teach high school students about game development, specifically in areas such as programming, design, graphics, sound, and time management.",
         "href": "https://aalborgungdomsskole.dk/gamelab-tirsdag/",
-        "image":"gamelab.png"
+        "image":"gamelab.webp"
     },
     {
         "name": "Power the evolution",
         "description": "Power The Evolution originated as a high school project, which subsequently progressed beyond its initial delivery stage with the continued involvement of myself and a friend.<br><br>The game received acclaim in the form of inclusion in Denmark's championship for game development and earned the \"Youth Against Global Warming\" award in 2018.",
         "href": null,
-        "image":"power.png"
+        "image":"power.webp"
     },
 ];
