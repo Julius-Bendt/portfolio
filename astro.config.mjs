18c9967d46322aa96bc31fb222d39d6ac4146fe4
bukz <julius@juto.dk>
2023-04-24 09:08:17 +0200

    Configuration issues should be fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
-
-import frontendistahtmlMinify from "@frontendista/astro-html-minify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://juto.dk",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    // compress(),
-    // frontendistahtmlMinify(),
-  ],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
